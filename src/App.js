434906ca2a3379170ab4c1f837e3e9334528f286
Humza Haseeb <hamzahaseeb5@gmail.com>
2022-08-03 01:53:52 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./imgs/logo.png";
+import Card from "./Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="logo">
+        <img src={logo} alt="icansee logo" />
+      </div>
+      <Card />
+    </>
   );
 }
 
